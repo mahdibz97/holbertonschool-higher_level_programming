bc305a64dc2553a86cd173267dc42f1549eaf552
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-04-30 21:33:48 +0100

    my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,17 +5,18 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-    if argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
+    arg = argv[2]    
+    if arg != '+' and arg != '-' and arg != '*' and arg != '/':
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
     a = int(argv[1])
     b = int(argv[3])
-    if argv[2] == '+':
+    if arg == '+':
         summ = add(a, b)
-    if argv[2] == '-':
+    if arg == '-':
         summ = sub(a, b)
-    if argv[2] == '*':
+    if arg == '*':
         summ = mul(a, b)
-    if argv[2] == '/':
+    if arg == '/':
         summ = div(a, b)
-    print('{} {} {} = {}'.format(a, argv[2], b, summ))
+    print('{} {} {} = {}'.format(a, arg, b, summ))
