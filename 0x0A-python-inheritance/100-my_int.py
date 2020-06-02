2f9d04f279375b4cffa601f884a9b18c419215df
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-06-02 23:04:50 +0100

    class MyInt that inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python3
+"""Define my class"""
+
+
+class MyInt(int):
+    """class MyInt"""
+
+    def __ne__(self, other):
+        """ne"""
+        return int(self) == int(other)
+
+    def __eq__(self, other):
+        """rq"""
+        return int(self) != int(other)
