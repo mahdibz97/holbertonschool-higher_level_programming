4064063d5b3b806a879ab77cf1135cd321fb0c9b
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-06-03 21:43:21 +0100

    fix

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+"""DEfine class"""
 
-"""A script that adds all arguments to a Python list, and then save them
-to a file."""
 
-from sys import argv
-from os import path
+import os
+import json
+
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
