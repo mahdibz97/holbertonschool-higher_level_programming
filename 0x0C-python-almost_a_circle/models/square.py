d6b43f61b0540946737fd383c54f43dd5e543b7f
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-06-09 15:40:04 +0100

    fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,7 +4,9 @@
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
+    """class square  that inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """init"""
         super().__init__(size, size, x, y, id)
