9d5ef95ffb068b60c52e9fa07c37c9ba2fec1e4a
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-06-09 16:06:18 +0100

    Update the class Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,3 +26,21 @@ class Square(Rectangle):
         """size setter"""
         self.width = size
         self.height = size
+
+    def update(self, *args, **kwargs):
+        """Update class Square by adding public method assigns attributes"""
+        for i in range(len(args)):
+            if i == 0:
+                self.id = args[0]
+            if i == 1:
+                self.width = args[1]
+                self.height = args[1]
+            if i == 2:
+                self.x = args[2]
+            if i == 3:
+                self.y = args[3]
+        else:
+            for key, val in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, val)
+           
