6c7d2920745177b550f18c7a1cd849e93abe86a5
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-07-14 19:42:31 +0100

    SQL more queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
--- user_0d_1
--- user_0d_2
+-- lists all privileges of the MySQL users user_0d_1  on your server (in localhost).
+-- lists all privileges of user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
