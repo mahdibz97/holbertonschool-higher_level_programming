56388d4e60e6060af5810894577011c009939d8e
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-09-16 13:18:02 +0100

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorialize (numb) {
-  if (numb === 0 || numb === 1 || (isNaN(process.argv[2]))){ 
-      return 1; 
-    }else{
-      return numb * factorialize(numb-1)
+  if (numb === 0 || numb === 1 || (isNaN(process.argv[2]))) {
+    return 1;
+  } else {
+    return numb * factorialize(numb - 1);
   }
 }
 console.log(factorialize(process.argv[2]));
