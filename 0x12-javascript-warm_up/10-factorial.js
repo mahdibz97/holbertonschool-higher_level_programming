02b9dda91841304db450db8297df9fb66289e589
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-09-16 11:21:33 +0100

    script that computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-function factorialize (num) {
-  if (num === 0 || num === 1 || (isNaN(process.argv[2])))
+function factorialize (numb) {
+  if (numb === 0 || numb === 1 || (isNaN(process.argv[2])))
     return 1;
-  for (let i = num - 1; i >= 1; i--) {
-    num *= i;
+  for (let i = numb - 1; i >= 1; i--) {
+    numb *= i;
   }
-  return num;
+  return numb;
 }
 console.log(factorialize(process.argv[2]));
